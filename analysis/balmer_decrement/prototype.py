78e7d6a28f9941cab0374a8ea190e8afc2bc852f
luiz <luiz_15_51@hotmail.com>
2022-12-24 21:34:58 -0300

    Adding plot of Balmer decreament estimation

--- analysis/balmer_decrement/prototype.py
+++ analysis/balmer_decrement/prototype.py
@@ -12,11 +12,12 @@ import json
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import interpolate
 
 file_metadata = ('../../data_products/toy_trick/MilesAgeMh/'
-                  'ppxf_emission_line_binned300_2components/'
-                  'metadata.json'
-                  )
+                 'ppxf_emission_line_binned300_2components/'
+                 'metadata.json'
+                 )
 
 file_output = ('../../data_products/toy_trick/MilesAgeMh/'
                'ppxf_emission_line_binned300_2components/'
@@ -29,18 +30,53 @@ with open(file_output) as fp:
 with open(file_metadata) as fp:
     out_metadata = json.load(fp)
 
-
+gas_names = np.asarray(out_ppxf['results']['gas_names'])
 corrected_flux = np.asarray(out_ppxf['results']['corrected_flux'])
+
+xbin = out_metadata['obs']['xbin']
+ybin = out_metadata['obs']['ybin']
+
 ha_1 = corrected_flux[8]
 hb_1 = corrected_flux[9]
-
-balmer_dec_1 = ha_1/hb_1
+bal_dec_1 = ha_1/hb_1
 
 ha_2 = corrected_flux[18]
 hb_2 = corrected_flux[19]
+bal_dec_2 = ha_2/hb_2
 
-balmer_dec_2 = ha_2/hb_2
+# Scatter plot
+fig, ax = plt.subplots(1, 2)
+ax[0].scatter(xbin, ybin, c=bal_dec_1)
+ax[1].scatter(xbin, ybin, c=bal_dec_2)
 
+#  Plot of interpolation
 
-plt.hist(balmer_dec_1, alpha=0.3, bins=5)
-plt.hist(balmer_dec_2, alpha=0.3, bins=5)
+inter = interpolate.LinearNDInterpolator
+# inter = interpolate.RBFInterpolator
+
+x_full = np.asarray(out_metadata['obs']['x_full'])
+y_full = np.asarray(out_metadata['obs']['y_full'])
+grid = np.column_stack((x_full, y_full))
+
+_interp_rbf_bal_dec_1 = inter(
+    np.column_stack([ybin, xbin]), bal_dec_1)
+
+rbf_bal_dec_1 = _interp_rbf_bal_dec_1(grid)
+
+_interp_rbf_bal_dec_2 = inter(
+    np.column_stack([ybin, xbin]), bal_dec_2)
+
+rbf_bal_dec_2 = _interp_rbf_bal_dec_2(grid)
+
+shape = (len(set(y_full.round(5))),
+         len(set(x_full.round(5))))
+
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(rbf_bal_dec_1.reshape(shape), origin='lower')
+ax[1].imshow(rbf_bal_dec_2.reshape(shape), origin='lower')
+
+#%%
+# Interpolation with selection
+fig, ax = plt.subplots(1, 2)
+ax[0].scatter(xbin, ybin, c=bal_dec_1)
+ax[1].scatter(xbin, ybin, c=bal_dec_2)
