34f4b668bb7f069627c4da544fb66a2b10bf6728
luiz <luiz_15_51@hotmail.com>
2023-02-24 19:55:09 -0300

    Provisional changes

--- exploration/run_ppxf_emission/main.py
+++ exploration/run_ppxf_emission/main.py
@@ -50,10 +50,10 @@ class Main:
         self.create_output_folder()
         self.keep_conf_copy()
         self.data = DataPreprocessing(self.meta)
-        self.execution = ExecutePpxf(self.data, self.meta)
-        self.meta_to_json()
-        self.results_to_json()
-        self.results_to_map()
+        # self.execution = ExecutePpxf(self.data, self.meta)
+        # self.meta_to_json()
+        # self.results_to_json()
+        # self.results_to_map()
 
     def read_config(self):
         with open(self.conf_file) as f:
@@ -142,6 +142,11 @@ class Main:
 
 
 if __name__ == '__main__':
-    conf = sys.argv[1]
-    ppxf_control = Main(conf)
-    ppxf_control.run_all()
+    # conf = sys.argv[1]
+    # ppxf_control = Main(conf)
+    # ppxf_control.run_all()
+
+# Debug
+    conf = 'test.yaml'
+    ppxf_prep = Main(conf)
+    ppxf_prep.run_all()
