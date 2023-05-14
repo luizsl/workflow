f2431bdca1428756f0a22ad199db6d51ab0e823d
luiz <luiz_15_51@hotmail.com>
2023-05-14 17:11:26 +0100

    Rename folder

--- miscellaneous/regularization_degree.py
+++ miscellaneous/regularization_degree.py
@@ -9,19 +9,22 @@ import multiprocessing as mp
 import subprocess
 
 def run_test(sn, regul, script_path, index):
-    conf_file = f'assess_regularization/sn{sn}_regul{regul}_fov1x5.yaml'
-    p0 = subprocess.run(f"nohup python {script_path} {conf_file} > nohup{index}.out",
-                   shell=True, check=True,
-                   )
+    conf_file = f'eval_regularization/sn{sn}_regul{regul}_fov1x5.yaml'
+    p0 = subprocess.run(
+        f"nohup python {script_path} {conf_file} > nohup{index}.out",
+        shell=True, check=True)
 
-limit = 1
+if __name__ == '__main__':
+    limit = 1
 
-with mp.Pool(processes=limit) as pool:
-    sn = 40
-    regul = [0, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05]
-    for index, r in enumerate(regul):
-        r = round(r, 2)
-        r = f'{r:0.2f}'
-        r = r.replace('.', 'd')
-        script_path = '../exploration/run_ppxf/main.py'
-        pool.apply(run_test, args=[sn, r, script_path, index])
+    with mp.Pool(processes=limit) as pool:
+        sn = 500
+        # regul = [0, 0.05, 0.04, 0.03, 0.02, 0.01]
+        regul = [0, 0.01]
+        for index, r in enumerate(regul):
+            r = round(r, 2)
+            r = f'{r:0.2f}'
+            r = r.replace('.', 'd')
+            print(r)
+            # script_path = '../exploration/run_ppxf/main.py'
+            # pool.apply(run_test, args=[sn, r, script_path, index])
