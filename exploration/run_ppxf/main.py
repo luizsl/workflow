e2071514eef2a7bd9db93094f0769e43b1af68fb
luiz <luiz_15_51@hotmail.com>
2023-05-15 00:35:24 +0100

    Removing no longer necessary chunks

--- exploration/run_ppxf/main.py
+++ exploration/run_ppxf/main.py
@@ -120,23 +120,6 @@ class Main:
             except Exception as e:
                 self.logger.info(str(e))
 
-    # def compute_secondary(self):
-    #     base = self.meta['output_run_ppxf']
-    #     datapath = os.path.join(base, 'weights.fits')
-    #     metadatapath = os.path.join(base, 'metadata.json')
-    #     stellar = PopMeanProperties(
-    #         datapath=datapath,
-    #         metadatapath=metadatapath,
-    #         age_log10=self.data.model.meta['age_log10'])
-
-    #     if 'mean_log_age_light' in self.meta['output']['secondary']:
-    #         stellar.save(stellar.log10_age_light, 'mean_log10_age_light',
-    #                      self.meta['output_run_ppxf'])
-
-    #     if 'mean_mh_light' in self.meta['output']['secondary']:
-    #         stellar.save(stellar.mh_light, 'mean_mh_light',
-    #                      self.meta['output_run_ppxf'])
-
 
 if __name__ == '__main__':
     # conf = sys.argv[1]
