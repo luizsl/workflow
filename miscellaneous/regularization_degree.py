f92eb4edd660295629aca6fe0151a8a9bfaf35c1
luiz <luiz_15_51@hotmail.com>
2023-05-18 11:54:34 +0100

    Update

---
+++ miscellaneous/regularization_degree.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 26 13:00:39 2022
+
+@author: Luiz
+"""
+import multiprocessing as mp
+import subprocess
+
+def run_test(sn, regul, script_path, index):
+    conf_file = f'eval_regularization/sn{sn}_regul{regul}_fov1x3_norm_obs.yaml'
+    p0 = subprocess.run(
+        f"nohup python {script_path} {conf_file} > nohup{index}.out",
+        shell=True, check=True)
+
+if __name__ == '__main__':
+    limit = 1
+
+    with mp.Pool(processes=limit) as pool:
+        sn = 100
+        regul = [0, 0.05, 0.04, 0.03, 0.02, 0.01]
+        for index, r in enumerate(regul):
+            r = round(r, 2)
+            r = f'{r:0.2f}'
+            r = r.replace('.', 'd')
+            print(r)
+            script_path = '../exploration/run_ppxf/main.py'
+            pool.apply(run_test, args=[sn, r, script_path, index])
---
+++ miscellaneous/regularization_statistics.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 28 13:04:39 2022
+
+@author: Luiz
+"""
+
+import os
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy.io import fits
+
+def read_fits_data(file, unit=0):
+    with fits.open(file) as hdul:
+        data = hdul[unit].data
+        return np.asarray(data)
+    
+def find_regul(regul, 
+               root_directory='../data_products/regularization_ngc613',
+               filename_pattern=None): 
+    name = filename_pattern.replace('[regul]', str(regul))
+    for path in Path(root_directory).rglob(name):
+        return path.parent.as_posix()
+
+if __name__ == '__main__':
+    reguls=[0,20,30,40,50,60,100]
+    filename_pattern = 'sn100_regul[regul]_fov1x5.yaml'
+    
+    # Read the processed galaxy observation    
+    galaxy_root_dir = find_regul(0, filename_pattern=filename_pattern)
+    galaxy_filename = os.path.join(galaxy_root_dir, 'galaxy.fits')
+    galaxy = read_fits_data(galaxy_filename)
+    
+    # Read bestfit of all tests
+    for regul in reguls:
+        root_dir = find_regul(regul, filename_pattern=filename_pattern)
+        filename = os.path.join(root_dir, 'bestfit.fits')
+        print(filename)
+        # locals()[f'bestfit_regul{regul}'] = read_fits_data(filename)
+    
+    # # Compute mse (mean squared error)
+    # for regul in reguls:
+    #     filename = os.path.join(root_dir, 'bestfit.fits')
+    #     locals()[f'mse_{regul}'] = np.nanmean(
+    #         (locals()[f'bestfit_regul{regul}'] - galaxy)**2,
+    #         axis=0)
+    #     plt.subplots()
+    #     plt.imshow(locals()[f'mse_{regul}'], vmin=0.001, vmax=0.2)
